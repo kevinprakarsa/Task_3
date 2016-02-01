655aafe280d1ac8d11b05d933a3239502ba45e27
kevinprakarsa <kevin.prakarsa@yahoo.com>
2016-02-01 22:14:02 +0700

    Update AppStore.java

--- Exercise_1/AppStore.java
+++ Exercise_1/AppStore.java
@@ -1,1 +1,28 @@
-
+/* 
+Nama 	: Kevin Prakarsa B R
+NIM  	: 1301144169
+Kelas	: IF-38-09 
+*/
+	 
+public class AppStore {
+ private Application[] appList = new Application[100];
+ private int countApp=0;
+	
+ public void createNewApp(String appName, int appSize) {
+  Application app = new Application(appName, appSize);
+   appList[sumApp] = app;
+   sumApp++;
+  }
+	
+ public Application getApp(int id) {
+  return appList[id];
+ }
+ 
+ public int getSumApp() {
+	 return sumApp;
+ }
+	
+ public String toString() {
+  return ("There are "+countApp+" applications ready to install");
+ }
+}
