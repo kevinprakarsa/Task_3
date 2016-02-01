bbc7d05e9223d85239e5806123e34e372e14fa99
kevinprakarsa <kevin.prakarsa@yahoo.com>
2016-02-01 22:15:20 +0700

    Update Application.java

--- Exercise_1/Application.java
+++ Exercise_1/Application.java
@@ -1,1 +1,27 @@
-
+/* 
+Nama 	: Kevin Prakarsa B R
+NIM  	: 1301144169
+Kelas	: IF-38-09 
+*/
+	 
+public class Application {
+ private String AppName;
+ private int AppSize;
+	
+ public Application(String AppName, int AppSize) {
+  this.AppName = AppName;
+  this.AppSize = AppSize;
+ }
+	
+ public String getAppName() {
+  return AppName;
+ }
+ 	
+ public int getAppSize() {
+  return AppSize;
+ }
+ 	
+ public String toString() {
+  return ("Application name : "+AppName+", with size : "+AppSize);
+ }
+}
